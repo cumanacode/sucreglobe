4907ee536b62555a9487d2c8f0185457c353c8cc
Roberto <rducharne2@gmail.com>
2024-02-03 22:13:18 -0400

    agregando el login

--- Config/Config.php
+++ Config/Config.php
@@ -9,7 +9,7 @@
 
 	//Datos de conexión a Base de Datos
 	const DB_HOST = "localhost";
-	const DB_NAME = "octubre23";
+	const DB_NAME = "test";
 	const DB_USER = "root";
 	const DB_PASSWORD = "";
 	const DB_CHARSET = "utf8";
