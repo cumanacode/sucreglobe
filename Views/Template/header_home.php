72f9689f2bfaa399c643c8a6201f72b96cddcac2
Roberto <rducharne2@gmail.com>
2024-02-16 14:27:37 -0400

    agregando el icono y moviendo las el titulo a la izquierda

--- Views/Template/header_home.php
+++ Views/Template/header_home.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Agency - Start Bootstrap Theme</title>
+        <title>Sucre Globe</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
@@ -15,6 +15,7 @@
         <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="<?= base_url(); ?>/Assets/css/styles.css" rel="stylesheet" />
+        <link rel="icon" class="js-site-favicon" type="image/svg+xml" href="<?= base_url(); ?>/Assets/images/avion.png">
     </head>
 <body id="page-top">
 
@@ -38,7 +39,7 @@
         </nav>
 
         <header class="masthead">
-            <div class="container">
+            <div class="container d-sm-flex flex-sm-column align-items-sm-start">
                 <div class="masthead-subheading">Welcome To Our Studio!</div>
                 <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
                 <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
